fb85b4a6a185964c474599fe8fc60eb87ef80e19
10xdevian <vikram.kr.gupta01@gmail.com>
2025-08-17 02:13:40 +0530

    refactor:(zod schema phone number min max added)

--- packages/zod-schema/src/schema/authSchema.ts
+++ packages/zod-schema/src/schema/authSchema.ts
@@ -4,7 +4,10 @@ export const signupSchema = z.object({
   username: z.string().min(3, "Name must be at least 3 characters"),
   email: z.string().email("Invalid email"),
   password: z.string().min(6, "Password must be at least 6 characters"),
-  number: z.string(),
+  number: z
+    .string()
+    .min(10, "Phone Number must be at least 10 Digits")
+    .max(14, "Must be a valid mobile number"),
   acceptTerms: z
     .boolean()
     .refine((val) => val === true, "You must accept terms"),
