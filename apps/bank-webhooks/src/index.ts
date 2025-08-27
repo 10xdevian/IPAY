49f64027c530970f6ad1aa9ba9bf4db255f6cb44
10xdevian <vikram.kr.gupta01@gmail.com>
2025-08-28 00:12:30 +0530

    feat :(adding banking webhooks )

---
+++ apps/bank-webhooks/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import db from "@repo/db/client";
+
+const app = express();
+//  add zod
+//  add types
+
+app.post("/hdfcWebhook", async (req, res) => {
+  const paymentInfo = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInfo.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInfo.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInfo.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    res.status(200).json({
+      msg: "captured",
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "Error while Processing webHooks",
+    });
+  }
+});
+
+app.listen(8080);
